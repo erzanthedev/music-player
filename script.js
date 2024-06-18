bde3b96cc9e5f2d3b5ced57ee7752fe7ded687b8
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-06-19 06:53:21 +1000

    feat: added function to update progress bar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -127,6 +129,17 @@ function nextSong() {
   playMusic();
 }
 
+// Update Prgress Bar and Time
+function updateProgressBar(event) {
+  if (isPlaying) {
+    const { currentTime, duration } = event.srcElement;
+    // Update Progress Bar Width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+}
+
 // Event Listener
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
